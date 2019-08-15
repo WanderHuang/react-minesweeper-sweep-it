2a21014553808ba3d44259dcc8c868bae3f323b6
wander <junjiehuang@yeah.net>
2019-08-15 10:26:20 +0800

    Updates

---
+++ precache-manifest.b104280dedcfe48e364bdf18d235cd31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e06397d03ceee99aea89db80388639",
+    "url": "/react-minesweeper-sweep-it/index.html"
+  },
+  {
+    "revision": "5d0c030a6a349603d03f",
+    "url": "/react-minesweeper-sweep-it/static/css/main.68fbd6f3.chunk.css"
+  },
+  {
+    "revision": "48c6019bba4e838d7c8c",
+    "url": "/react-minesweeper-sweep-it/static/js/2.52f73b58.chunk.js"
+  },
+  {
+    "revision": "5d0c030a6a349603d03f",
+    "url": "/react-minesweeper-sweep-it/static/js/main.8049e6ac.chunk.js"
+  },
+  {
+    "revision": "6d67882fbb7162961766",
+    "url": "/react-minesweeper-sweep-it/static/js/runtime~main.6654ed91.js"
+  }
+]);
